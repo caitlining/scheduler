2accff74a545aa06ced782a5d6df15cb04df6443
caitlining <caitlincing@gmail.com>
2019-10-28 13:43:13 -0700

    Implement formatSpots function to properly format the number of interview spots available, ensure function allows test to pass for this component

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,7 +8,17 @@ export default function DayListItem(props) {
   const dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
- });
+  });
+
+  const formatSpots = function () {
+    let spotsRemaining;
+    if (props.spots === 0) {
+      spotsRemaining = 'no spots remaining';
+    } else if (props.spots > 0) {
+      spotsRemaining = `${props.spots}${props.spots === 1 ? ' spot ': ' spots '} remaining`;
+    }
+    return spotsRemaining;
+  }
 
   return (
     <li 
@@ -16,7 +26,7 @@ export default function DayListItem(props) {
       onClick = {() => props.setDay(props.name)}
     >
       <h2 className="text--regular">{ props.name }</h2>
-      <h3 className="text--light">{ props.spots }</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 };
